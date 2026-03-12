0856c2ea8ae6e4acf39e12e1e1689baf27734b5a
winterlyembrace <4icken00@gmail.com>
2026-03-13 04:18:20 +1000

    added password hash for login via virsh console

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,8 @@ variable "ssh_public_key" {
   description = "Public SSH key for user egor"
   type        = string
 }
+
+variable "user_password_hash" {
+  description = "Password hash for logging into VMs"
+  type	      = string
+}
