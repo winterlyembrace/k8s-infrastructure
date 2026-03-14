349705878c26ebf30b96450c2792b31f2e7d2b83
winterlyembrace <4icken00@gmail.com>
2026-03-15 07:44:28 +1000

    changed hardcoded ip to variable

--- terraform/cloud-init.tf
+++ terraform/cloud-init.tf
@@ -2,17 +2,17 @@ resource "libvirt_cloudinit_disk" "commoninit" {
   for_each = merge(var.k8s_nodes, var.edge_nodes, var.infra_nodes)
 
   name = "commoninit-${each.key}.iso"
-  pool = "default" 
+  pool = "default"
 
   user_data = templatefile("${path.module}/templates/user-data.tftpl", {
-    hostname      = each.key 
-    ssh_key       = var.ssh_public_key
-    password_hash = var.user_password_hash
+    hostname = each.key
+    ssh_key  = var.ssh_public_key
   })
 
   network_config = templatefile("${path.module}/templates/network-config.tftpl", {
-    ip_address = each.value.ip
-    gateway    = each.key == "router" ? null : "192.168.100.2"
+    ip_address     = each.value.ip
+    gateway        = each.key == "bastion" ? null : var.edge_nodes.bastion.ip
+    bastion_ext_ip = var.bastion_ip_config.bastion_ext_ip
   })
 
   meta_data = jsonencode({
