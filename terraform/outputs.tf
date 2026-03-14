5633f97e374d3e4c8f0aa86ee48b464468660212
winterlyembrace <4icken00@gmail.com>
2026-03-15 03:39:03 +1000

    fix router external ip variable

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,7 @@
-output "router_external_ip" {
-  value = libvirt_domain.nodes["router"].network_interface[0].addresses[0]
+resource "local_file" "ansible_inventory" {
+  content = templatefile("${path.module}/templates/inventory.tftpl", {
+    router_external_ip = "192.168.122.252" 
+    nodes     = var.k8s_nodes          
+  })
+  filename = "../ansible/inventory.ini"
 }
