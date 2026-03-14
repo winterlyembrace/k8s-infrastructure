9eafd45cd1e378eb2a642ea91223e224c0ec880a
winterlyembrace <4icken00@gmail.com>
2026-03-15 05:24:21 +1000

    fixed outputs.tf for ansible

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,23 @@
+resource "local_file" "ansible_config" {
+  content  = <<-EOT
+    [defaults]
+    inventory = inventory.ini
+    host_key_checking = False
+    remote_user = egor
+    roles_path = ./roles
+
+    [ssh_connection]
+    pipelining = True
+    ssh_args = -o ControlMaster=auto -o ControlPersist=60s
+  EOT
+  filename = "../ansible/ansible.cfg"
+}
+
+
 resource "local_file" "ansible_inventory" {
   content = templatefile("${path.module}/templates/inventory.tftpl", {
     router_external_ip = "192.168.122.252" 
-    nodes     = var.k8s_nodes          
+    nodes     = merge(var.k8s_nodes, var.edge_nodes, var.infra_nodes)
   })
   filename = "../ansible/inventory.ini"
 }
