f53468befe4c66ba7ec100cb08b470f1481b25f7
winterlyembrace <4icken00@gmail.com>
2026-03-14 22:58:29 +1000

    moved cloudinit_disk to separated file

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,26 +13,6 @@ provider "libvirt" {
 
 
 
-resource "libvirt_cloudinit_disk" "commoninit" {
-  for_each = merge(var.k8s_nodes, var.edge_nodes, var.infra_nodes)
-
-  name = "commoninit-${each.key}.iso"
-  pool = "default" 
-
-  user_data = templatefile("${path.module}/cloud_init.tftpl", {
-    hostname      = each.key 
-    ssh_key       = var.ssh_public_key
-    password_hash = var.user_password_hash
-  })
-
-  meta_data = jsonencode({
-    "instance-id"    = each.key
-    "local-hostname" = each.key
-  })
-}
-
-
-
 # Base image
 
 resource "libvirt_volume" "ubuntu_base" {
