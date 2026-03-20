8310ce2c748dccf4d998b1331116f16b30228e05
winterlyembrace <4icken00@gmail.com>
2026-03-21 00:33:19 +1000

    Add provider versions

--- terraform/modules/kvm_instance/versions.tf
+++ terraform/modules/kvm_instance/versions.tf
@@ -2,22 +2,22 @@ terraform {
   required_providers {
     tls = {
       source  = "hashicorp/tls"
-      version = "~= 4.0.0"
+      version = "~> 4.0.0"
     }
 
     null = {
       source  = "hashicorp/null"
-      version = "~= 3.0.0"
+      version = "~> 3.0.0"
     }
 
     local = {
       source  = "hashicorp/local"
-      version = "~= 2.0.0"
+      version = "~> 2.0.0"
     }
 
     libvirt = {
       source  = "dmacvicar/libvirt"
-      version = "~= 0.7.1"
+      version = "~> 0.7.1"
     }
   }
 }
