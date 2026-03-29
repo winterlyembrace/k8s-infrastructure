c5251b6aced216fffea57ca322686f8633d7b12d
winterlyembrace <4icken00@gmail.com>
2026-03-29 10:33:20 +1000

    Fix ansible inventory output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,20 +5,20 @@ output "ansible_inventory" {
         ansible_user = "egor"
       }
       children = {
-          k8s_masters = {
-            hosts = {
-              for name, node in module.kvm_instance : name => {
-                ansible_host = node.internal_ip
-              } if length(regexall("master", name)) > 0
-            }
-           }
-          k8s_workers = {
-            hosts = {
-              for name, node in module.kvm_instance : name => {
-                ansible_host = node.internal_ip
-              } if length(regexall("worker", name)) > 0
-            }
+        k8s_masters = {
+          hosts = {
+            for name, node in module.kvm_instance : name => {
+              ansible_host = node.internal_ip
+            } if length(regexall("master", name)) > 0
           }
+        }
+        k8s_workers = {
+          hosts = {
+            for name, node in module.kvm_instance : name => {
+              ansible_host = node.internal_ip
+            } if length(regexall("worker", name)) > 0
+          }
+        }
       }
     }
   }
